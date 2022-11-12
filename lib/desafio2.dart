f18869c52752cd724539c470dec1377f30c439cb
FredericoWP <frederico.wallace@yahoo.com>
2022-11-12 16:55:04 -0300

    add clipReact

--- lib/desafio2.dart
+++ lib/desafio2.dart
@@ -57,23 +57,26 @@ class _WidgetExpansiveState extends State<WidgetExpansive> {
           trailing: AnimatedRotation(
             turns: isExpanded ? 0 : 0.5,
             duration: const Duration(milliseconds: 250),
-            child: const Icon(Icons.arrow_drop_down),
+            child: const Icon(Icons.arrow_drop_up),
           ),
         ),
-        AnimatedAlign(
-          alignment: Alignment.topCenter,
-          duration: const Duration(seconds: 5),
-          heightFactor: 1,
-          child: SizedBox(
-            child: !isExpanded
-                ? null
-                : Column(
-                    children: const [
-                      FlutterLogo(size: 50),
-                      Text(
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
-                    ],
-                  ),
+        //if (isExpanded)
+        ClipRect(
+          child: AnimatedAlign(
+            alignment:
+                isExpanded ? Alignment.topCenter : Alignment.bottomCenter,
+            duration: const Duration(seconds: 1),
+            heightFactor: isExpanded ? 1 : 0,
+            child: Container(
+              color: Colors.grey[100],
+              child: Column(
+                children: const [
+                  FlutterLogo(size: 50),
+                  Text(
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                ],
+              ),
+            ),
           ),
         ),
       ],
